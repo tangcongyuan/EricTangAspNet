2725fcb8e83c075aa12adeb3e3b7087f7fde1606
Eric (Congyuan) Tang <tangcongyuan@gmail.com>
2017-11-22 15:54:48 -0500

    Main layout done

---
+++ EricTangAspNet/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EricTangAspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Portfolio()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+    }
+}
--- EricTangAspNet/Startup.cs
+++ EricTangAspNet/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +11,7 @@ namespace EricTangAspNet
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +21,11 @@ namespace EricTangAspNet
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
+            app.UseStaticFiles(); // For the wwwroot folder
+            app.UseNodeModules(env);
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
